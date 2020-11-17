b2911d0b23618c85c1b7119bb27a2705934b98db
Dan Aument <danaument@gmail.com>
2020-11-17 13:56:28 -0600

    test api calls are working

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,26 +1,35 @@
-
-
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=austin&appid=f5f233cfe3163b61d82d69907e8f50ea"
+var cityName = "austin"
+var apiKey = "f5f233cfe3163b61d82d69907e8f50ea"
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=austin&appid=f5f233cfe3163b61d82d69907e8f50ea"
 
 
 
 //api call
 var getCityWeather = function () {
     $.ajax({
-        url: queryURL,
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`,
         method: "GET"
     }).then(function(response) {
-        consol.log(response);
-    
+        console.log(response);
+        console.log(response.coord.lon);
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        console.log(lon, lat);
+
         // UVI API => pass coordinates from weather API to UVI API
-        // $.ajax({}).then(function(response){
-    
-        // });
-    
+        $.ajax({
+            url: `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+        });
+        
         // // 5 day forcast 
-        // $.ajax({}).then(function(response){
-    
-        // });
+        $.ajax({
+            url: `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+        }).then(function(response){
+            console.log(response);
+        });
     });
 }
 
